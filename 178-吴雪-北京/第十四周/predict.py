fb69ea361590229017461c735d310403d83d3d63
ajunwiwx <102153920+ajunwiwx@users.noreply.github.com>
2024-07-25 11:54:48 +0800

    Add files via upload

---
+++ 178-吴雪-北京/第十四周/predict.py
@@ -0,0 +1,15 @@
+from frcnn import FRCNN
+from PIL import Image
+
+frcnn = FRCNN()
+
+while True:
+    try:
+        image = Image.open('img/street.jpg')
+    except:
+        print('Open Error! Try again!')
+        continue
+    else:
+        r_image = frcnn.detect_image(image)
+        r_image.show()
+frcnn.close_session()
