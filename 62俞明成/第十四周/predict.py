4dae1138bef004feb4a1c5d1e9294d9e715b8c61
cnkey02 <123292084+cnkey02@users.noreply.github.com>
2024-07-29 20:05:41 +0800

    Add files via upload

---
+++ 62俞明成/第十四周/predict.py
@@ -0,0 +1,17 @@
+from frcnn import FRCNN
+from PIL import Image
+
+frcnn = FRCNN()
+
+while True:
+    img = input('img/street.jpg')
+    try:
+        image = Image.open('img/street.jpg')
+        print('1')
+    except:
+        print('Open Error! Try again!')
+        continue
+    else:
+        r_image = frcnn.detect_image(image)
+        r_image.show()
+frcnn.close_session()
