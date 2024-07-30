b36aea20a70413d4f171a8f4ebcbb6af0a0b29a2
lpj <17316177703@163.com>
2024-07-30 18:34:08 +0800

    Changes

--- 22-李朋举/WEEK11/alexnet/AlexNet-Keras-master/predict.py
+++ 22-李朋举/WEEK11/alexnet/AlexNet-Keras-master/predict.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
     img_nor = np.expand_dims(img_nor,axis = 0)
     # 使用resize_image函数对图片进行了大小调整，使其符合模型的输入要求
     img_resize = utils.resize_image(img_nor,(224,224))
-    #utils.print_answer(np.argmax(model.predict(img)))
+    # utils.print_answer(np.argmax(model.predict(img)))
     # 代码使用模型对调整大小后的图片进行了预测，并打印出预测结果。预测结果是一个整数->转换成中文，表示图片所属的类别。
     print(utils.print_answer(np.argmax(model.predict(img_resize))))
     cv2.imshow("ooo",img)
