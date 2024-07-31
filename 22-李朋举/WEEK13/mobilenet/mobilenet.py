354a17ca5379a86ac0582aa7605d5e2d1609762d
lpj <17316177703@163.com>
2024-07-31 22:10:03 +0800

    2024-07 keras实现MobileNet 训练、预测

--- 22-李朋举/WEEK13/mobilenet/mobilenet.py
+++ 22-李朋举/WEEK13/mobilenet/mobilenet.py
@@ -71,15 +71,19 @@ def MobileNet(input_shape=[224, 224, 3],  # 输入图像的形状大小为 224x2
 
     return model
 
-def _conv_block(inputs, filters, kernel=(3, 3), strides=(1, 1)):
-    x = Conv2D(filters,
-               kernel,
-               padding='same',
-               use_bias=False,
-               strides=strides,
+
+'''
+定义了一个卷积块函数 `_conv_block`，用于在卷积神经网络中构建卷积层、批量归一化层和 ReLU 激活函数。
+'''
+def _conv_block(inputs, filters, kernel=(3, 3), strides=(1, 1)):  # `inputs`：输入张量，通常是上一层的输出
+    x = Conv2D(filters,  # 卷积核的数量，决定了输出特征图的深度
+               kernel,  # 卷积核的大小，默认为 `(3, 3)`
+               padding='same',  # 卷积操作会在输入的周围添加额外的零值，使得输出的特征图大小与输入的大小相同。这样做的目的是为了避免在卷积过程中丢失边缘信息，同时保持输出的空间维度与输入一致
+               use_bias=False,  # 卷积层中不使用偏置项。这意味着卷积操作的输出仅取决于输入和卷积核的权重.
+               strides=strides,  # `strides`：卷积的步长，默认为 `(1, 1)`
                name='conv1')(inputs)
-    x = BatchNormalization(name='conv1_bn')(x)
-    return Activation(relu6, name='conv1_relu')(x)
+    x = BatchNormalization(name='conv1_bn')(x)  # 对卷积层的输出进行批量归一化，以加速训练并提高模型的稳定性
+    return Activation(relu6, name='conv1_relu')(x)  # 应用 ReLU 激活函数，将输入值限制在非负范围内，增加模型的非线性表达能力
 
 
 def _depthwise_conv_block(inputs, pointwise_conv_filters,
@@ -131,9 +135,9 @@ def relu6(x):
 
 
 def preprocess_input(x):
-    x /= 255.
-    x -= 0.5
-    x *= 2.
+    x /= 255.  # 将输入数据 x 除以 255，进行归一化处理，使得输入值在 0 到 1 之间
+    x -= 0.5  # 将归一化后的输入数据 x 减去 0.5，进行中心化处理，使得输入值的均值为 0
+    x *= 2.  # 将中心化后的输入数据 x 乘以 2，进行缩放处理，使得输入值的范围在 -1 到 1 之间
     return x
 
 
@@ -141,12 +145,25 @@ if __name__ == '__main__':
     model = MobileNet(input_shape=(224, 224, 3))
 
     img_path = 'elephant.jpg'
+    # <PIL.Image.Image image mode=RGB size=224x224 at 0x2B65B0A70B8>
     img = image.load_img(img_path, target_size=(224, 224))
+    '''
+    ndarray=(224,224,3) [[[146. 149. 154.],  [147. 150. 155.],  [148. 151. 156.],  ...,  [130. 136. 148.], 
+    '''
     x = image.img_to_array(img)
+    '''
+    ndarray=(1,224,224,3) [[[[146. 149. 154.],   [147. 150. 155.],   [148. 151. 156.],   ...,   [130. 136. 148.],  
+    '''
     x = np.expand_dims(x, axis=0)
+    '''
+    ndarray=(1,224,224,3) [[[[ 0.14509809  0.1686275   0.20784318],   [ 0.15294123  0.17647064  0.21568632],   [ 0.16078436  0.18431377  0.22352946],   ...,   [ 0.0196079   0.06666672  0.16078436], 
+    '''
     x = preprocess_input(x)
     print('Input image shape:', x.shape)
 
+    '''
+    ndarray=(1,1000)  [[1.50127313e-10 1.46900873e-12 4.05713463e-10 1.69053885e-11, 
+    '''
     preds = model.predict(x)
-    print(np.argmax(preds))
-    print('Predicted:', decode_predictions(preds, 1))  # 只显示top1
+    print(np.argmax(preds))  # 386
+    print('Predicted:', decode_predictions(preds, 1))  # 只显示top1  Predicted: [[('n02504458', 'African_elephant', 0.7636121)]]
