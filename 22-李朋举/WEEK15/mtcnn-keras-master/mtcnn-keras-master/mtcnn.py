9a9effb7b571aa6a099180292ac5b1d5ce6bfea7
lpj <17316177703@163.com>
2024-08-05 23:45:41 +0800

    2024-08 mtcnn

--- 22-李朋举/WEEK15/mtcnn-keras-master/mtcnn-keras-master/mtcnn.py
+++ 22-李朋举/WEEK15/mtcnn-keras-master/mtcnn-keras-master/mtcnn.py
@@ -1,45 +1,96 @@
-from keras.layers import Conv2D, Input,MaxPool2D, Reshape,Activation,Flatten, Dense, Permute
+from keras.layers import Conv2D, Input, MaxPool2D, Reshape, Activation, Flatten, Dense, Permute
 from keras.layers.advanced_activations import PReLU
 from keras.models import Model, Sequential
 import tensorflow as tf
 import numpy as np
 import utils
 import cv2
-#-----------------------------#
-#   粗略获取人脸框
-#   输出bbox位置和是否有人脸
-#-----------------------------#
+
+'''   
+      (f,f)       h-f+2p         w-f+2p
+(h,w) ------>  ( ------- + 1 ,  -------- + 1)
+       s,p           s              s
+'''
+
+
+# -------------------------------------------------------------------------------------#
+# 1. 第一层P-Net将经过卷积，池化操作后输出分类（对应像素点是否存在人脸）和回归（回归box)结果。
+#    粗略获取人脸框
+#    输出bbox位置和是否有人脸
+# -------------------------------------------------------------------------------------#
 def create_Pnet(weight_path):
+
+    # 输入（12, 12, 3）
     input = Input(shape=[None, None, 3])
 
+    # 添加第一个卷积层，卷积核大小为(3, 3)，输出通道数为 10，步长为 1，边界填充为valid。
+    '''
+             (3,3)
+    12x12x3 -------> (12-3+1 , 12-3+1) = (10 , 10) , 输出通道由卷积核决定, ∴ （10, 10, 10）
+              1,1
+    '''
     x = Conv2D(10, (3, 3), strides=1, padding='valid', name='conv1')(input)
-    x = PReLU(shared_axes=[1,2],name='PReLU1')(x)
+    # 添加 PReLU 激活函数，共享轴为[1, 2]。
+    x = PReLU(shared_axes=[1, 2], name='PReLU1')(x)
+    # 添加最大池化层，池化窗口大小为 2。
+    '''
+    (10/2, 10/2, 10 )
+    '''
     x = MaxPool2D(pool_size=2)(x)
 
+    # 添加第二个卷积层，卷积核大小为(3, 3)，输出通道数为 16，步长为 1，边界填充为valid。
+    '''
+             (3,3)
+    5x5x10 -------> (5-3+1 , 5-3+1) = (3 , 3) , 输出通道由卷积核决定, ∴ （3, 3, 16）
+              1,1
+    '''
     x = Conv2D(16, (3, 3), strides=1, padding='valid', name='conv2')(x)
-    x = PReLU(shared_axes=[1,2],name='PReLU2')(x)
+    # 添加 PReLU 激活函数，共享轴为[1, 2]
+    x = PReLU(shared_axes=[1, 2], name='PReLU2')(x)
 
+    # 添加第三个卷积层，卷积核大小为(3, 3)，输出通道数为 32，步长为 1，边界填充为valid。
+    '''
+             (3,3)
+    3x3x16 -------> (3-3+1 , 3-3+1) = (1 , 1) , 输出通道由卷积核决定, ∴ （1, 1, 32）
+              1,1
+    '''
     x = Conv2D(32, (3, 3), strides=1, padding='valid', name='conv3')(x)
-    x = PReLU(shared_axes=[1,2],name='PReLU3')(x)
+    # 添加 PReLU 激活函数，共享轴为[1, 2]。
+    x = PReLU(shared_axes=[1, 2], name='PReLU3')(x)
 
+    # 添加分类层，卷积核大小为(1, 1)，输出通道数为 2，激活函数为softmax。
     classifier = Conv2D(2, (1, 1), activation='softmax', name='conv4-1')(x)
-    # 无激活函数，线性。
+    # 加边界框回归层，卷积核大小为(1, 1)，输出通道数为 4。 无激活函数，线性。
     bbox_regress = Conv2D(4, (1, 1), name='conv4-2')(x)
 
+    # 创建模型，将输入层和输出层连接起来。
     model = Model([input], [classifier, bbox_regress])
+    # 加载预训练的权重
     model.load_weights(weight_path, by_name=True)
     return model
 
-#-----------------------------#
+
+# -----------------------------#
 #   mtcnn的第二段
 #   精修框
-#-----------------------------#
+# -----------------------------#
 def create_Rnet(weight_path):
+
+    # 接收一个大小为24x24x3的输入图像
     input = Input(shape=[24, 24, 3])
+
     # 24,24,3 -> 11,11,28
+    '''
+    添加第一个卷积层，卷积核大小为3x3，输出通道数为 28，步长为 1，边界填充为valid。
+    输出形状：(22, 22, 28) （计算过程：(24 - 3 + 1) = 22，(24 - 3 + 1) = 22，卷积核数量为 28）
+    '''
     x = Conv2D(28, (3, 3), strides=1, padding='valid', name='conv1')(input)
     x = PReLU(shared_axes=[1, 2], name='prelu1')(x)
-    x = MaxPool2D(pool_size=3,strides=2, padding='same')(x)
+    '''
+    添加最大池化层，池化窗口大小为 3，步长为 2，边界填充为same。
+    输出形状：(11, 11, 28) （计算过程：(22 - 3) / 2 + 1 = 11，(22 - 3) / 2 + 1 = 11）
+    '''
+    x = MaxPool2D(pool_size=3, strides=2, padding='same')(x)
 
     # 11,11,28 -> 4,4,48
     x = Conv2D(48, (3, 3), strides=1, padding='valid', name='conv2')(x)
@@ -49,83 +100,106 @@ def create_Rnet(weight_path):
     # 4,4,48 -> 3,3,64
     x = Conv2D(64, (2, 2), strides=1, padding='valid', name='conv3')(x)
     x = PReLU(shared_axes=[1, 2], name='prelu3')(x)
+
     # 3,3,64 -> 64,3,3
-    x = Permute((3, 2, 1))(x)
-    x = Flatten()(x)
+    x = Permute((3, 2, 1))(x)  # 对特征图进行重排，将通道维度移到最后。
+    x = Flatten()(x)  # 将特征图展平为一维向量
     # 576 -> 128
-    x = Dense(128, name='conv4')(x)
-    x = PReLU( name='prelu4')(x)
+    x = Dense(128, name='conv4')(x)  # 添加一个全连接层，神经元数量为 128。
+    x = PReLU(name='prelu4')(x)
+
     # 128 -> 2 128 -> 4
-    classifier = Dense(2, activation='softmax', name='conv5-1')(x)
-    bbox_regress = Dense(4, name='conv5-2')(x)
-    model = Model([input], [classifier, bbox_regress])
-    model.load_weights(weight_path, by_name=True)
+    classifier = Dense(2, activation='softmax', name='conv5-1')(x)  # 添加分类层，神经元数量为 2，激活函数为softmax。
+    bbox_regress = Dense(4, name='conv5-2')(x)  # 添加边界框回归层，神经元数量为 4。
+    model = Model([input], [classifier, bbox_regress])  # 创建模型，将输入层和输出层连接起来。
+    model.load_weights(weight_path, by_name=True)  # 加载预训练的权重。
     return model
 
-#-----------------------------#
+
+# -----------------------------#
 #   mtcnn的第三段
 #   精修框并获得五个点
-#-----------------------------#
+# -----------------------------#
 def create_Onet(weight_path):
-    input = Input(shape = [48,48,3])
+
+    input = Input(shape=[48, 48, 3])
+
     # 48,48,3 -> 23,23,32
     x = Conv2D(32, (3, 3), strides=1, padding='valid', name='conv1')(input)
-    x = PReLU(shared_axes=[1,2],name='prelu1')(x)
+    x = PReLU(shared_axes=[1, 2], name='prelu1')(x)
     x = MaxPool2D(pool_size=3, strides=2, padding='same')(x)
+
     # 23,23,32 -> 10,10,64
     x = Conv2D(64, (3, 3), strides=1, padding='valid', name='conv2')(x)
-    x = PReLU(shared_axes=[1,2],name='prelu2')(x)
+    x = PReLU(shared_axes=[1, 2], name='prelu2')(x)
     x = MaxPool2D(pool_size=3, strides=2)(x)
+
     # 8,8,64 -> 4,4,64
     x = Conv2D(64, (3, 3), strides=1, padding='valid', name='conv3')(x)
-    x = PReLU(shared_axes=[1,2],name='prelu3')(x)
+    x = PReLU(shared_axes=[1, 2], name='prelu3')(x)
     x = MaxPool2D(pool_size=2)(x)
+
     # 4,4,64 -> 3,3,128
     x = Conv2D(128, (2, 2), strides=1, padding='valid', name='conv4')(x)
-    x = PReLU(shared_axes=[1,2],name='prelu4')(x)
+    x = PReLU(shared_axes=[1, 2], name='prelu4')(x)
     # 3,3,128 -> 128,12,12
-    x = Permute((3,2,1))(x)
+    x = Permute((3, 2, 1))(x)
 
     # 1152 -> 256
     x = Flatten()(x)
-    x = Dense(256, name='conv5') (x)
+    x = Dense(256, name='conv5')(x)
     x = PReLU(name='prelu5')(x)
 
     # 鉴别
     # 256 -> 2 256 -> 4 256 -> 10 
-    classifier = Dense(2, activation='softmax',name='conv6-1')(x)
-    bbox_regress = Dense(4,name='conv6-2')(x)
-    landmark_regress = Dense(10,name='conv6-3')(x)
+    classifier = Dense(2, activation='softmax', name='conv6-1')(x)
+    bbox_regress = Dense(4, name='conv6-2')(x)
+    landmark_regress = Dense(10, name='conv6-3')(x)
 
     model = Model([input], [classifier, bbox_regress, landmark_regress])
     model.load_weights(weight_path, by_name=True)
 
     return model
 
+
 class mtcnn():
     def __init__(self):
+        '''
+        1. 第一层P-Net将经过卷积，池化操作后输出分类（对应像素点是否存在人脸）和回归（回归box)结果。
+        '''
         self.Pnet = create_Pnet('model_data/pnet.h5')
+        '''
+        2. 第二层网络将第一层输出的结果使用非极大抑制（NMS）来去除高度重合的候选框，并将这些候选框放入R-Net中进行精细的操作，拒绝大量错误框，
+           再对回归框做校正，并使用NMS去除重合框，输出分支同样两个分类和回归。
+        '''
         self.Rnet = create_Rnet('model_data/rnet.h5')
+        '''
+        3. 最后将R-Net输出认为是人脸的候选框输入到O-Net中再一次进行精细操作，拒绝掉错误的框，
+            此时输出分支包含三个分类：
+            a. 是否有人脸：2个输出；
+            b. 回归：回归得到的框的起始点（或中心点）的xy坐标和框的长宽，4个输出；
+            c. 人脸特征点定位：5个人脸特征点的xy坐标，10个输出。
+        '''
         self.Onet = create_Onet('model_data/onet.h5')
 
     def detectFace(self, img, threshold):
-        #-----------------------------#
+        # -----------------------------#
         #   归一化，加快收敛速度
         #   把[0,255]映射到(-1,1)
-        #-----------------------------#
+        # -----------------------------#
         copy_img = (img.copy() - 127.5) / 127.5
         origin_h, origin_w, _ = copy_img.shape
-        #-----------------------------#
+        # -----------------------------#
         #   计算原始输入图像
         #   每一次缩放的比例
-        #-----------------------------#
+        # -----------------------------#
         scales = utils.calculateScales(img)
 
         out = []
-        #-----------------------------#
+        # -----------------------------#
         #   粗略计算人脸框
         #   pnet部分
-        #-----------------------------#
+        # -----------------------------#
         for scale in scales:
             hs = int(origin_h * scale)
             ws = int(origin_w * scale)
@@ -140,7 +214,7 @@ class mtcnn():
         rectangles = []
         for i in range(image_num):
             # 有人脸的概率
-            cls_prob = out[i][0][0][:,:,1]
+            cls_prob = out[i][0][0][:, :, 1]
             # 其对应的框的位置
             roi = out[i][1][0]
 
@@ -149,7 +223,8 @@ class mtcnn():
             out_side = max(out_h, out_w)
             print(cls_prob.shape)
             # 解码过程
-            rectangle = utils.detect_face_12net(cls_prob, roi, out_side, 1 / scales[i], origin_w, origin_h, threshold[0])
+            rectangle = utils.detect_face_12net(cls_prob, roi, out_side, 1 / scales[i], origin_w, origin_h,
+                                                threshold[0])
             rectangles.extend(rectangle)
 
         # 进行非极大抑制
@@ -158,10 +233,10 @@ class mtcnn():
         if len(rectangles) == 0:
             return rectangles
 
-        #-----------------------------#
+        # -----------------------------#
         #   稍微精确计算人脸框
         #   Rnet部分
-        #-----------------------------#
+        # -----------------------------#
         predict_24_batch = []
         for rectangle in rectangles:
             crop_img = copy_img[int(rectangle[1]):int(rectangle[3]), int(rectangle[0]):int(rectangle[2])]
@@ -180,10 +255,10 @@ class mtcnn():
         if len(rectangles) == 0:
             return rectangles
 
-        #-----------------------------#
+        # -----------------------------#
         #   计算人脸框
         #   onet部分
-        #-----------------------------#
+        # -----------------------------#
         predict_batch = []
         for rectangle in rectangles:
             crop_img = copy_img[int(rectangle[1]):int(rectangle[3]), int(rectangle[0]):int(rectangle[2])]
@@ -199,4 +274,3 @@ class mtcnn():
         rectangles = utils.filter_face_48net(cls_prob, roi_prob, pts_prob, rectangles, origin_w, origin_h, threshold[2])
 
         return rectangles
-
