4ef609473d47b7e9afbe752c5f66a520052baff4
Zenowang0 <163700024+Zenowang0@users.noreply.github.com>
2024-08-16 21:17:19 +0800

    Add files via upload

--- 198-王祖能-上海/240718 第十二周作业/Resnet50.py
+++ 198-王祖能-上海/240718 第十二周作业/Resnet50.py
@@ -1,5 +1,6 @@
 '''
-残差结构50层，增加网络层数的同时，减少过拟合
+residual net残差结构50层，增加网络层数的同时，减少信息损失过拟合，相比于VGG16/19层数上明显提高
+网络结构理论上可根据结果自己调整，这里由于直接调用了训练好的.h权重参数，网络结构必须一致，否则权重不匹配，不能推理。
 '''
 import numpy as np
 from keras import layers
@@ -17,20 +18,22 @@ def conv_block(input_tensor, kernel_size, filters, stage, block, strides=(2, 2))
     filter1, filter2, filter3 = filters  # filters是一个列表，1~3分别对应[0],[1],[2]
     conv_name_base = 'res' + str(stage) + block + '_branch'
     bn_name_base = 'bn' + str(stage) + block + '_branch'
-    x = Conv2D(filter1, (1, 1), strides=strides, name=conv_name_base + '2a')(input_tensor)  # filter1表示卷积后维度
+    x = Conv2D(filter1, (1, 1), strides=strides, name=conv_name_base + '2a')(input_tensor)  # filter1表示卷积后维度, 默认padding='valid'
     x = BatchNormalization(name=bn_name_base + '2a')(x)
     x = Activation('relu')(x)
-    # 单个整数或由两个整数构成的list / tuple，卷积核的宽度和长度。如为单个整数，则表示各空间维度的相同长度
+
     x = Conv2D(filter2, kernel_size, padding='same', name=conv_name_base + '2b')(x)
     x = BatchNormalization(name=bn_name_base + '2b')(x)
     x = Activation('relu')(x)
-    x = Conv2D(filter3, (1, 1), name=conv_name_base + '2c')(x)  # padding='valid',
+
+    x = Conv2D(filter3, (1, 1), name=conv_name_base + '2c')(x)
     x = BatchNormalization(name=bn_name_base + '2c')(x)
 
     shortcut = Conv2D(filter3, (1, 1), strides=strides, name=conv_name_base + '1')(input_tensor)
     shortcut = BatchNormalization(name=bn_name_base + '1')(shortcut)
+    # conv 2a层和shortcut层strides=(2, 2), conv 2b/2c层strides=(1, 1)，保证了shortcut和conv图像大小一致
 
-    x = layers.add([x, shortcut])
+    x = layers.add([x, shortcut])  # conv 2a层和shortcut层都用了filter3, 保证结果的(h, w, c)一致，可以add
     x = Activation('relu')(x)
     return x
     pass
@@ -40,7 +43,7 @@ def identity_block(input_tensor, kernel_size, filters, stage, block):
     filter1, filter2, filter3 = filters
     conv_name_base = 'res' + str(stage) + block + '_branch'
     bn_name_base = 'bn' + str(stage) + block + '_branch'
-    x = Conv2D(filter1, (1, 1), name=conv_name_base + '2a')(input_tensor)
+    x = Conv2D(filter1, (1, 1), name=conv_name_base + '2a')(input_tensor)  # 默认缺省参数strides=(1, 1), padding='valid'
     x = BatchNormalization(name=bn_name_base + '2a')(x)
     x = Activation('relu')(x)
     x = Conv2D(filter2, kernel_size, padding='same', name=conv_name_base + '2b')(x)
@@ -49,7 +52,7 @@ def identity_block(input_tensor, kernel_size, filters, stage, block):
     x = Conv2D(filter3, (1, 1), name=conv_name_base + '2c')(x)
     x = BatchNormalization(name=bn_name_base + '2c')(x)
 
-    x = layers.add([x, input_tensor])
+    x = layers.add([x, input_tensor])  # 由于identityblock的shortcut没有处理，所以conv 2c层的filter3要和输入input的通道数相同
     x = Activation('relu')(x)
     return x
     pass
@@ -87,8 +90,8 @@ def resnet50(input_shape=(224, 224, 3), num_classes=1000):
     x = identity_block(x, 3, [512, 512, 2048], stage=5, block='b')
     x = identity_block(x, 3, [512, 512, 2048], stage=5, block='c')
 
-    x = AveragePooling2D(pool_size=(7, 7), name='avg_pool')(x)
-    x = Flatten()(x)
+    x = AveragePooling2D(pool_size=(7, 7), name='avg_pool')(x)  # strides if None，默认为pool_size=(7, 7)
+    x = Flatten()(x)  # 注意格式
     x = Dense(num_classes, activation='softmax', name='fc1000')(x)
 
     model = Model(img_input, x, name='resnet50')
@@ -99,13 +102,13 @@ def resnet50(input_shape=(224, 224, 3), num_classes=1000):
 
 if __name__ == '__main__':
     model = resnet50()
-    model.summary()  # 显示构建模型的层级关系，bias应该就是不训练的参数
+    model.summary()  # 显示构建模型的层级关系，统计各种参数量，bias应该就是不训练的参数
     img1_path = 'elephant.jpg'
     img2_path = 'bike.jpg'
-    img = image.load_img(img1_path, target_size=(224, 224))  # 从指定路径加载图像，并将其调整为指定的大小(224, 224)
-    print(img.size)
+    img = image.load_img(img2_path, target_size=(224, 224))  # 从指定路径加载图像，并将其调整为指定的大小(224, 224)
+    print(type(img), img.size)  # 此时还是图片，没有shape, <class 'PIL.Image.Image'> (224, 224)
     x = image.img_to_array(img)  # [224, 224, 3]
-    print(x.shape)
+    print(x.shape)  # 图片转化成数组，且是浮点型数据
     x = np.expand_dims(x, axis=0)  # [1, 224, 224, 3]
     print(x.shape)
     x = preprocess_input(x)  # 图像标准化归一化，缩小差异提高准确度和收敛速度
